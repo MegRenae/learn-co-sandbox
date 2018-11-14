a712b442acb8906c80fb87af5566ce0508bb2eb8
MEGAN RENAE LEWIS <meganrenaelewis@gmail.com>
2018-11-14 19:52:14 +0000

    automatically backed up by learn

--- greeting.rb
+++ greeting.rb
@@ -1,9 +1,13 @@
-def greeting 
-  puts "Hello World"
-end  
-
-greeting
-greeting
-greeting
-greeting
-greeting
+def say_hello_world_ten_times
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
